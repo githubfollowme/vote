211800c825b8cfb0bf0fa04f25be663a5fcfde88
moonfollowme <flymayday112@gmail.com>
2021-11-29 14:32:39 +0800

    整理後 新增老師課堂練習檔案

---
+++ frontend/show_vote_list.php
@@ -0,0 +1,16 @@
+<h1>列出所有的問題</h1>
+<?php
+$subjects=all('topics');
+echo "<ol>";
+foreach ($subjects as $key => $value) {
+    if(rows('options',['topic_id'=>$value['id']]) > 0){
+    echo "<li>";
+    echo "<a href='index.php?do=vote&id={$value['id']}'>";
+    echo $value['topic'];
+    echo "</a>";
+    echo "</li>";
+}
+}
+echo "</ol>";
+
+?>
